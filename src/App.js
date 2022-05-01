c07bd9bec8ffd44400fa6e00a829edcbb481240e
Yash Mahale <yashmahale1702@gmail.com>
2022-05-01 13:12:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { HomePage } from './pages/homepage/homepage.component'
+import { Routes, Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>Hats Page</div>
+  )
+}
 
 function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/hats' element={<HatsPage />} />
+      </Routes>
+
+    </div>
   );
 }
 
